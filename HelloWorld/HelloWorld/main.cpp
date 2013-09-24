805871621db03038ff738d2e2b7ab0c40b00821e
Alix1723 <alix1723@live.co.uk>
2013-09-24 10:37:04 +0000

    Moifying text within main.cpp

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hello World, press enter key to exit"<<endl;
-	cout<<"I am a code monkey"<<endl;
+	cout<<"Hello World, press the enter key to exit"<<endl;
+	cout<<"NEXT TIME ON GAME GRUMPS"<<endl;
 	cin.ignore();
 	
 	return 0;
